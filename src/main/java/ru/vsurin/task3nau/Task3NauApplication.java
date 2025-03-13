8434ace24a371ad1641b94eab813475e895647cd
Totheskysx10 <crazycat87654@gmail.com>
2025-03-13 21:44:48 +0500

    Initial commit

---
+++ src/main/java/ru/vsurin/task3nau/Task3NauApplication.java
@@ -0,0 +1,13 @@
+package ru.vsurin.task3nau;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Task3NauApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Task3NauApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ru/vsurin/task3nau/Task3NauApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.vsurin.task3nau;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Task3NauApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
