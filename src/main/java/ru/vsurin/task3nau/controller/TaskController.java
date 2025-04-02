ac7b7f6f093cd563ea941fa85990c5c7eff776bf
Totheskysx10 <crazycat87654@gmail.com>
2025-04-02 23:07:22 +0500

    добавил контроллер на транзакционную операцию

--- src/main/java/ru/vsurin/task3nau/controller/TaskController.java
+++ src/main/java/ru/vsurin/task3nau/controller/TaskController.java
@@ -4,7 +4,9 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import ru.vsurin.task3nau.domain.Project;
 import ru.vsurin.task3nau.domain.Task;
+import ru.vsurin.task3nau.exception.TaskNotFoundException;
 import ru.vsurin.task3nau.repository.custom.TaskCustomRepository;
+import ru.vsurin.task3nau.service.TaskService;
 
 import java.util.List;
 
@@ -16,9 +18,11 @@ import java.util.List;
 public class TaskController {
 
     private final TaskCustomRepository taskCustomRepository;
+    private final TaskService taskService;
 
-    public TaskController(TaskCustomRepository taskCustomRepository) {
+    public TaskController(TaskCustomRepository taskCustomRepository, TaskService taskService) {
         this.taskCustomRepository = taskCustomRepository;
+        this.taskService = taskService;
     }
 
     /**
@@ -35,5 +39,19 @@ public class TaskController {
         }
 
         return ResponseEntity.ok(tasks);
+    }
+
+    /**
+     * Удаляет задачу и все связанные комментарии
+     * @param id идентификатор
+     */
+    @DeleteMapping("/del-task-with-comments")
+    public ResponseEntity<String> deleteTaskWithComments(@RequestParam Long id) {
+        try {
+            taskService.deleteTaskWithComments(id);
+            return ResponseEntity.ok().build();
+        } catch (TaskNotFoundException e) {
+            return ResponseEntity.notFound().build();
+        }
     }
 }
